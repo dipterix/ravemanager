42201511789aa260fe9532d5201e053867aa4390
dipterix <dipterix.wang@gmail.com>
2023-07-04 05:19:40 -0400

    gh fix

--- tests/testthat/test-installation.R
+++ tests/testthat/test-installation.R
@@ -1,15 +1,14 @@
 # DIPSAUS DEBUG START
 # library(testthat)
 test_that("Installation works", {
+  message(Sys.getenv("TEST_INSTALLATION"))
   if(as.character(Sys.getenv("TEST_INSTALLATION")) == "TRUE") {
-
     ravemanager::install()
     libpath <- .libPaths()[[1]]
     expect_true(nzchar(system.file(package = "ravebuiltins", lib.loc = libpath)))
-    expect_true(requireNamespace(package = "raveio", lib.loc = libpath))
+    expect_true(nzchar(system.file(package = "rpyANTs", lib.loc = libpath)))
 
     ravemanager::configure_python()
-    expect_true(requireNamespace(package = "rpyANTs", lib.loc = libpath))
     rpyANTs <- asNamespace("rpyANTs")
     expect_s3_class(rpyANTs$ants, c("ants.proxy", "python.builtin.module"))
   }
