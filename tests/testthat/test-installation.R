f75acf2101dee65e376ef2711a7bb41d5dc3b611
dipterix <dipterix.wang@gmail.com>
2023-07-04 05:33:57 -0400

    full check

--- tests/testthat/test-installation.R
+++ tests/testthat/test-installation.R
@@ -1,7 +1,7 @@
 # DIPSAUS DEBUG START
 # library(testthat)
 test_that("Installation works", {
-  if(toupper(as.character(Sys.getenv("TEST_INSTALLATION"))) == "TRUE") {
+  if(toupper(as.character(Sys.getenv("TEST_INSTALLATION"))) %in% c("TRUE", "YES")) {
     ravemanager::install()
     libpath <- .libPaths()[[1]]
     expect_true(nzchar(system.file(package = "ravebuiltins", lib.loc = libpath)))
