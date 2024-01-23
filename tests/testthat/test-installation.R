5df3a8b368cbe2659623f02602eafafd88653f13
dipterix <dipterix.wang@gmail.com>
2024-01-23 14:01:22 -0500

    removed tests; using installer to test

--- tests/testthat/test-installation.R
+++ tests/testthat/test-installation.R
@@ -1,28 +1,2 @@
 # DIPSAUS DEBUG START
 # library(testthat)
-
-remove_conda <- function() {
-  try(silent = TRUE, {
-    rpymat <- asNamespace("rpymat")
-    root <- normalizePath(rpymat$install_root(), mustWork = FALSE)
-    unlink(root, recursive = TRUE, force = TRUE)
-  })
-}
-test_that("Installation works", {
-  if(detect_gh_ci()) {
-    ravemanager::install()
-    libpath <- .libPaths()[[1]]
-    expect_true(nzchar(system.file(package = "ravebuiltins", lib.loc = libpath)))
-    expect_true(nzchar(system.file(package = "rpyANTs", lib.loc = libpath)))
-    rpyANTs <- asNamespace("rpyANTs")
-    rpymat <- asNamespace("rpymat")
-
-    on.exit({
-      remove_conda()
-    })
-
-    # remove_conda()
-    # ravemanager::configure_python()
-    # expect_s3_class(rpyANTs$ants, c("ants.proxy", "python.builtin.module"))
-  }
-})
